877f4e08b83562af8e5699420e3e08caa699de20
Mikhail_Nikolaev <mikhail.nikolaev@wemaysell.com>
2020-07-31 16:08:09 +0300

    add comments

--- createMap.bat
+++ createMap.bat
@@ -3,15 +3,19 @@ rem 2. split *.osm data with splitter.jar. Result is multiple small *.osm files 
 rem 3. run this bat file. Result is imagesupp.img in output directory
 rem 4. see result in basecamp: mount virtual drive, copy imagesupp.img to VIRT_DRIVE:/Garmin/, use JaVaWa Device Manager to import map from virtual drive to BaseCamp, start BaseCamp
 
+rem If map is created with contours then create osm map and map with contours. Map with contours should be transparent and have priority>25(default). Then join 2 maps with gmt.
+rem Osm raw data and contours can be downloaded from bbbike or another sites.
+
 call %GPS_MAP_BUILDER_PROPERTIES_DIR%\map_builder_properties.cmd
 
 if %CREATE_MAKE_TRANSPARRENT% == true (
+rem use   --transparent --draw-priority=30^  for map with contours
   set TRANSPARRENT=--transparent --draw-priority=30
 ) else (
   set TRANSPARRENT=
 )
 rem order is significant. -c template.args should be last
-
+rem family-id is same as FID in OSM-2018.txt
 java -jar %CREATE_MKGMAP_DIR%\mkgmap.jar^
   --gmapsupp^
   --style-file=%CREATE_CONFIG_DIR%^
@@ -19,7 +23,7 @@ java -jar %CREATE_MKGMAP_DIR%\mkgmap.jar^
   --code-page=1251^
   --output-dir=%CREATE_DESTINATION_DIR%^
   %TRANSPARRENT%^
-  --family-id=840^
+  --family-id=480^
   -c %CREATE_CONFIG_DIR%\optionsfile.args^
   --road-name-config=%CREATE_CONFIG_DIR%\roadNameConfig.txt^
   -c %SPLIT_DESTINATION_DIR%\template.args^
--- splitOsm.bat
+++ splitOsm.bat
@@ -1,6 +1,6 @@
 call %GPS_MAP_BUILDER_PROPERTIES_DIR%\map_builder_properties.cmd
 
-java.exe -Xmx1g -jar %SPLIT_SPLITTER_DIR%\splitter.jar^
+java.exe -Xmx2g -jar %SPLIT_SPLITTER_DIR%\splitter.jar^
   --mapid=%SPLIT_MAP_ID%^
   --description=%SPLIT_SOURCE_FILE_NAME%^
   --output-dir=%SPLIT_DESTINATION_DIR%^
